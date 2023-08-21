66606025729cd8284158c98f8ee203b481d9f316
Chi Nguyen <nglinhchi65@gmail.com>
2023-08-21 16:22:00 +0300

    Exercise completed

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [number1, setNumber1] = useState('0');
+  const [number2, setNumber2] = useState('0');
+  const [result, setResult] = useState('');
+
+  const plus = () => {
+    const sum = parseFloat(number1) + parseFloat(number2);
+    setResult(sum.toString())
+  }
+  const minus = () => {
+    const difference = parseFloat(number1) - parseFloat(number2);
+    setResult(difference.toString())
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Calculator: Result is {result}</Text>
+      
+      <TextInput style={{width: 200, borderColor:'grey', borderWidth:1, height: 32, marginTop:10, padding:5}}
+                  onChangeText={ number1 => setNumber1(number1)}
+                  keyboardType='numeric'/>
+      <TextInput style={{width: 200, borderColor:'grey', borderWidth:1, height: 32, marginTop:10, padding: 5}}
+                  onChangeText={ number2 => setNumber2(number2)}
+                  keyboardType='numeric'/>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.customButton}
+          onPress={plus}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.customButton}
+          onPress={minus}>
+          <Text style={styles.buttonText}>- </Text>
+        </TouchableOpacity>
+      </View>
+        
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +50,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  customButton: {
+    backgroundColor: 'purple', // Background color of the button
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'purple', // Border color of the button
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    margin: 5
+  },
+  buttonText: {
+    color: 'white', // Text color of the button
+    textAlign: 'center',}
 });
